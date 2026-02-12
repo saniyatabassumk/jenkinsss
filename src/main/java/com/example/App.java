3edd85d0974ef280716ddcf2aa30a756773abe99
saniyatabassumk <saniyatabassum537@gmail.com>
2026-02-12 17:22:52 +0530

    Update output message in App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,6 +5,6 @@ package com.example;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World from maven!");
     }
 }
